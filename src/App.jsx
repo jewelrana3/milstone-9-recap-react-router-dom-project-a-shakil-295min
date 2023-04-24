6820d2fc54445bbca32395cdb35b7efecfcf0870
jewelrana3 <juyelrana7752@gmail.com>
2023-04-24 08:46:12 +0600

    errorPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./components/Home"
+
 const App = () => {
-  return <h1>Welcome tddo HeroGadget</h1>
+  return <>
+    <Header></Header>
+    <div className="min-h-[calc(100vh-137px)]">
+      <Outlet></Outlet>
+    </div>
+    <Footer></Footer>
+  </>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import ErrorPage from './components/ErrorPage'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<App></App>,
+        errorElement:<ErrorPage></ErrorPage>,
+        children:[
+           {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/about',
+                element:<About></About>
+            }
+        ]
+    }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}></RouterProvider>
+)
